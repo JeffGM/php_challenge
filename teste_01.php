a921089a07b389e3c634f2e2802328c661a20cae
Jefferson <jeovaneml@live.com>
2021-01-12 17:05:40 -0300

    implemented a custom sum function

--- teste_01.php
+++ teste_01.php
@@ -1,1 +1,20 @@
 <?php
+
+function customSum($positiveInteger) {
+    if(!is_integer($positiveInteger))
+        throw new Exception("The input must be an integer!");
+    if($positiveInteger <= 0)
+        throw new Exception("The integer must be greater than 0!");
+
+    $sum = 0;
+
+    for($i = 1; $i < $positiveInteger; $i++) {
+        if($i % 3 == 0 || $i % 5 == 0)
+            $sum += $i;
+    }
+
+    return $sum;
+}
+
+$testNumber = 10;
+echo customSum($testNumber);
